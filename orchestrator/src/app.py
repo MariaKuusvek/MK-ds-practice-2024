fac60ed5a2c85dea26d11aade010cc8c3f8f25fb
anettehabanen <anette.habanen@gmail.com>
2024-02-27 16:10:00 +0200

    adding back greet() function

--- orchestrator/src/app.py
+++ orchestrator/src/app.py
@@ -25,9 +25,17 @@ sys.path.insert(0, utils_path)
 import suggestions_pb2 as suggestions
 import suggestions_pb2_grpc as suggestions_grpc
 
-
-
 import grpc
+
+def greet(name='you'):
+    # Establish a connection with the fraud-detection gRPC service.
+    with grpc.insecure_channel('fraud_detection:50051') as channel:
+        # Create a stub object.
+        stub = fraud_detection_grpc.HelloServiceStub(channel)
+        # Call the service through the stub object.
+        response = stub.SayHello(fraud_detection.HelloRequest(name=name))
+    return response.greeting
+
 
 def fraud_detection_func(info):
     # Establish a connection with the fraud-detection gRPC service.
