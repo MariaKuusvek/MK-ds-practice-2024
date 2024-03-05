d03ab3ccf7835d8ba5f2a7f45574bb0942e71f87
anettehabanen <anette.habanen@gmail.com>
2024-03-05 13:11:02 +0200

    Getting info from the threads in orchastrator

--- orchestrator/src/app.py
+++ orchestrator/src/app.py
@@ -25,6 +25,11 @@ import suggestions_pb2_grpc as suggestions_grpc
 
 import grpc
 
+fraud_detection_result = ''
+transaction_verification_result = ''
+books_suggestions_result = ''
+
+
 def greet():
     print("hello")
 
@@ -35,7 +40,10 @@ def fraud_detection_func(creditcard):
         stub = fraud_detection_grpc.FraudServiceStub(channel)
         # Call the service through the stub object.
         response = stub.FraudLogic(fraud_detection.FraudRequest(creditcardnr=creditcard))
-    return response.verdict
+
+    # Adding the result to the global variable
+    global fraud_detection_result
+    fraud_detection_result = response.verdict
 
 
 def transaction_verification_func(itemsL, name, contact, street, city, state, zip, country, ccnr, cvv, expdate):
@@ -53,14 +61,17 @@ def transaction_verification_func(itemsL, name, contact, street, city, state, zi
                                                                                        cvv = cvv,
                                                                                        expirationDate = expdate,))
         
-    return response.verdict
+    global transaction_verification_result
+    transaction_verification_result = response.verdict
+        
 
 def books_suggestion_func():
     with grpc.insecure_channel('suggestions:50053') as channel:
         stub = suggestions_grpc.SuggestionsServiceStub(channel)
         response = stub.SuggestionsLogic(suggestions.SuggestionsRequest())
-    return response.books
 
+    global books_suggestions_result 
+    books_suggestions_result = response
 
 
 
@@ -95,6 +106,8 @@ def checkout():
     # Print request object data
     print("Request Data:", request.json)
 
+    print(len(request.json['items']))
+
     thread_fraud = threading.Thread(target=fraud_detection_func, args=(request.json['creditCard']['number'],))
     thread_verification = threading.Thread(target=transaction_verification_func, args=(len(request.json['items']),
                                                                                         request.json['user']['name'],
@@ -117,14 +130,19 @@ def checkout():
     thread_verification.join()
     thread_books.join()
 
-    print("Fraud result")
-    print(thread_fraud.join())
+    final_verdict = ''
+
+    if fraud_detection_result != '' and transaction_verification_result != '' and books_suggestions_result != '' :
+        if fraud_detection_result == 'Not Fraud' and transaction_verification_result == 'Pass':
+            final_verdict = 'Order Approved'
+        else:
+            final_verdict = 'Order NOT Approved'
 
 
     # Dummy response following the provided YAML specification for the bookstore
     order_status_response = {
         'orderId': '12345',
-        'status': 'Order Approved',
+        'status': final_verdict,
         'suggestedBooks': [
             {'bookId': '123', 'title': 'Dummy Book 1', 'author': 'Author 1'},
             {'bookId': '456', 'title': 'Dummy Book 2', 'author': 'Author 2'}
