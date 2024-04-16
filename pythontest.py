54d6dd27e7a8fcd58873fdc5bab0d553d2b0fb98
MariaKuusvek <2020mariaku@gmail.com>
2024-04-16 15:42:47 +0300

    Test File testing

--- pythontest.py
+++ pythontest.py
@@ -1,7 +1,27 @@
-list = [1, 2, 3]
+import fileinput
+import sys
 
-print(list.index(2))
 
-list.insert(-1, 60)
 
-print(list)
+path = "testFile.txt"
+file = open(path, "r")
+
+lines = file.readlines()
+
+for line in lines:
+    print(line.strip())
+
+file.close()
+
+with open(path, 'w') as fp:
+    fp.write('\n'.join(["yay", "yey", "test"]))
+    print('Done')
+
+#for line in fileinput.input(file, inplace=1):
+#    book_info = line.split(",")
+#    if book_info[0] == title:
+#        new_quantity = book_info[1] - quantity
+#        new_line = title + "," + str(new_quantity)
+#        line = new_line
+#    sys.stdout.write(line)
+#file.close() 
