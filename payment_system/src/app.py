32b25acd44d551ff2d5c2961d8ccaba628bef213
MariaKuusvek <2020mariaku@gmail.com>
2024-04-19 13:05:51 +0300

    payment system service

---
+++ payment_system/src/app.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import grpc
+import fileinput
+from concurrent import futures
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# This set of lines are needed to import the gRPC stubs.
+# The path of the stubs is relative to the current file, or absolute inside the container.
+# Change these lines only if strictly needed.
+FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
+utils_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/payment_system'))
+sys.path.insert(0, utils_path)
+import payment_system_pb2 as payment_system
+import payment_system_pb2_grpc as payment_system_grpc
+
+
+# Create a class to define the server functions, derived from
+# payment_system_pb2_grpc.PaymentServiceServicer
+class PaymentService(payment_system_grpc.PaymentServiceServicer):
+
+    def paymentLogic(self, request, context):
+        response = payment_system.PaymentResponse()
+        response.verdict = "OK"
+        return response
+    
+def serve():
+    # Create a gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor())
+    # Add HelloService
+    payment_system_grpc.add_PaymentServiceServicer_to_server(PaymentService(), server)
+
+    # Listen on port 49667
+    port = "49667"
+    server.add_insecure_port("[::]:" + port)
+
+    # Start the server
+    server.start()
+    logging.info("Executor server started. Listening on port 49667.")
+    # Keep thread alive
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
